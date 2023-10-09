1b44293a77bde922c5286bcc785c7e4c5e7a8504
Paulo Lemes <paulo-lemes@live.com>
2023-10-08 21:03:49 -0300

    Front-End Ada
    Repositório de exercícios e projetos ref. curso

---
+++ projeto-ada-ifood/coding-tank/aulas/imc.js
@@ -0,0 +1,10 @@
+console.log('IMC')
+
+function calcularIMC () {
+const peso = document.getElementById("peso").value
+const altura = document.getElementById("altura").value
+
+const imc = peso/(altura**2)
+
+document.getElementById("resultado").textContent = imc.toFixed(2)
+}
